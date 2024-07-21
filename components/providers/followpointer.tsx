af8cd077089c8e5136f21b4ac383e288d3761bfa
HemanshuYPatil <hemanshuypatil@gmail.com>
2024-07-21 13:04:34 +0530

    intial

--- components/providers/followpointer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {motion,AnimatePresence,useMotionValue} from 'framer-motion'; 
-function FollowPointer({ info, x, y }: { x: number, y: number, info: { name: string, email: string, avatar: string } }) {
-  return (
-    <div>followpointer</div>
-  )
-}
-
-export default FollowPointer
